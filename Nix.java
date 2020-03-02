7622f179cddbed03e09b6ae5cb9dcc5beb1834e1
Rúben Domingues <51157774+rubendomingues@users.noreply.github.com>
2020-03-02 18:09:40 +0000

    Add files via upload

--- Nix.java
+++ Nix.java
@@ -29,27 +29,16 @@ public class Nix extends AdvancedRobot {
 
 		// Loop forever
 		while (true) {
-			turnGunRight(10); // Scans automatically
+			turnGunRight(360); // Scans automatically
 		}
 	}
-
+	/**
+	 * This function is called when he detects an opponent
+	 * It makes the robot to dodge from the enemy bullet
+	 * It makes the robot to fire to the enemy bullet
+	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
-		this.waitFire++;
-		double newEnergy = e.getEnergy();
-    	//Rotate 90 degrees from enemy
-    	setTurnRight(e.getBearing() + 90);
-    	if(newEnergy != this.enemyEnergy) {
-    		//Update enemy energy
-    		this.enemyEnergy = newEnergy;
-    		//Add random factor so it can't be easly predicted
-   		 	int randomInteger = -10 + (int) (Math.random() * ((10 - (-10)) + 1));
-   		 	if(randomInteger == 0)
-   		 		moveDirection*=-1;
-   		 	else
-   		 		moveDirection*= (randomInteger/Math.abs(randomInteger));
-   		 	//Move left and Right 
-   		 	setAhead(moveDirection*100);
-   	 	}
+    	this.dodgeEnemy(e);
 		// Calculate exact location of the robot
 		double absoluteBearing = getHeading() + e.getBearing();
 		double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
@@ -61,15 +50,13 @@ public class Nix extends AdvancedRobot {
     	
     	// Save all the coordinates enemy was
     	this.enemyCords.addFirst(new Point2D.Double(enemyX, enemyY));
-    	
 		// If it's close enough, fire!
 		if (Math.abs(bearingFromGun) <= 3) {
 			turnGunRight(bearingFromGun);
 			// We check gun heat here, because calling fire()
 			// uses a turn, which could cause us to lose track
 			// of the other robot.
-			if (getGunHeat() == 0 && this.waitFire >= 15) {
-				this.waitFire = 0;
+			if (getGunHeat() == 0) {
 				fire(Math.min(3 - Math.abs(bearingFromGun), getEnergy() - .1));
 			}
 		} // otherwise just set the gun to turn.
@@ -90,12 +77,27 @@ public class Nix extends AdvancedRobot {
    	 	this.enemyEnergy = event.getEnergy();
     }
     
+    //Walks away from the wall
     public void onHitWall(HitWallEvent e) {
     	setAhead(this.moveDirection*-1*100);
     }
     
-	public void onWin(WinEvent e) {
-		// Victory dance
-		turnRight(36000);
-	}
+    public void dodgeEnemy(ScannedRobotEvent e) {
+		double newEnergy = e.getEnergy();
+    	//Rotate 90 degrees from enemy
+    	setTurnRight(e.getBearing() + 90);
+    	if(newEnergy != this.enemyEnergy) {
+    		//Update enemy energy
+    		this.enemyEnergy = newEnergy;
+    		//Add random factor so it can't be easily predicted
+   		 	int randomInteger = -10 + (int) (Math.random() * ((10 - (-10)) + 1));
+   		 	if(randomInteger == 0)
+   		 		moveDirection*=-1;
+   		 	else
+   		 		moveDirection*= (randomInteger/Math.abs(randomInteger));
+   		 	//Move left and Right 
+   		 	setAhead(moveDirection*100);
+   	 	}
+    }
+    
 }				
